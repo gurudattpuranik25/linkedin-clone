4f2b39289bf3a77610379294fb09108516aa38d8
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-08-04 11:45:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div>I am building a linkedin clone!</div>;
+  return (
+    <div className=" text-3xl font-semibold text-sky-500">
+      I am building a linkedin clone!
+    </div>
+  );
 }
 
 export default App;
